627780889b4d7e060c7cad384e32f5756b772286
alexorbh7 <alexorbh7@hotmail.com>
2017-03-02 22:16:18 +0100

    Mirilla no terminada/ Fade hecho.
    Mirilla por terminar con su funcionamiento espécifico.
    Fade hecho.

---
+++ prePrototipo/Assets/Scripts/SightPointController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SightPointController : MonoBehaviour {
+
+    public Texture2D t_sightPoint;
+
+    void OnGUI()
+    {
+        GUI.Label( new Rect(Screen.width / 2, Screen.height / 2, Screen.height * 0.1f, Screen.width * 0.1f), t_sightPoint);
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
